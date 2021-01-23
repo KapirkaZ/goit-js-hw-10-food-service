88ff42b235863195e6aa85899930e0e44adc2249
Kapirka <68941822+KapirkaZ@users.noreply.github.com>
2021-01-23 21:57:50 +0200

    добавлены импорты в index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import './styles.css';
+import menuTemplate from './templates/template.hbs';
+import './changeTheme.js';
+import './styles.css';
+
+let menu = document.querySelector('.js-menu');
+
+function buildMenu(array) {
+  const markup = array.map(post => menuTemplate(post)).join('');
+  menu.insertAdjacentHTML('beforeend', markup);
+}
+
+buildMenu(menuElement);
